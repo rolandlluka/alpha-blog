2c9f63bf837461bea3da982c0e755d3274a1184f
rolandlluka <rolandlluka@gmail.com>
2017-06-22 23:42:11 +0200

    blog app in rails first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+  resources :articles
+
+  get 'signup', to: 'users#new'
+
+  resources :users, except: [:new]
+end
